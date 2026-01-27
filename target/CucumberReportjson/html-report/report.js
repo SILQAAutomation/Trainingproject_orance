f6802bd01bd088da2c9f32047c50187e2e7372bb
suraj.joggessur <rjoggessur@gmail.com>
2026-01-27 16:31:38 +0400

    Suraj's review

--- target/CucumberReportjson/html-report/report.js
+++ target/CucumberReportjson/html-report/report.js
@@ -93,7 +93,7 @@ formatter.examples({
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 51900,
+  "duration": 58900,
   "status": "passed"
 });
 formatter.scenario({
@@ -156,14 +156,14 @@ formatter.match({
   "location": "Steps.iAmOnOrangeMainPage(String)"
 });
 formatter.result({
-  "duration": 7357841000,
+  "duration": 8052050800,
   "status": "passed"
 });
 formatter.match({
   "location": "Steps.iVerifyLoginPage()"
 });
 formatter.result({
-  "duration": 1181577200,
+  "duration": 1373895200,
   "status": "passed"
 });
 formatter.match({
@@ -180,28 +180,29 @@ formatter.match({
   "location": "Steps.iInputUserNameAndPassword(String,String)"
 });
 formatter.result({
-  "duration": 33148600,
-  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//input[@placeholder\u003d\u0027Username\u0027]\"}\n  (Session info: chrome\u003d144.0.7559.97)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.4.0\u0027, revision: \u0027unknown\u0027, time: \u0027unknown\u0027\nSystem info: host: \u0027SIL-LT5-RAKSHAL\u0027, ip: \u0027130.1.19.197\u0027, os.name: \u0027Windows 11\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_371\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities [{networkConnectionEnabled\u003dfalse, chrome\u003d{chromedriverVersion\u003d144.0.7559.96 (d7b80622cfab91c265741194e7942eefd2d21811-refs/branch-heads/7559@{#3667}), userDataDir\u003dC:\\Users\\Raksha\\AppData\\Local\\Temp\\scoped_dir32620_1815234139}, timeouts\u003d{implicit\u003d0.0, pageLoad\u003d300000.0, script\u003d30000.0}, goog:processID\u003d21840.0, pageLoadStrategy\u003dnormal, unhandledPromptBehavior\u003ddismiss and notify, strictFileInteractability\u003dfalse, platform\u003dANY, proxy\u003dProxy(), webauthn:extension:minPinLength\u003dtrue, goog:chromeOptions\u003d{debuggerAddress\u003dlocalhost:57763}, webauthn:extension:credBlob\u003dtrue, webauthn:extension:prf\u003dtrue, acceptInsecureCerts\u003dfalse, browserVersion\u003d144.0.7559.97, fedcm:accounts\u003dtrue, browserName\u003dchrome, javascriptEnabled\u003dtrue, platformName\u003dwindows, setWindowRect\u003dtrue, webauthn:extension:largeBlob\u003dtrue, webauthn:virtualAuthenticators\u003dtrue}]\nSession ID: 250455ee345dd01bbd2b03f3fdb92938\n*** Element info: {Using\u003dxpath, value\u003d//input[@placeholder\u003d\u0027Username\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:150)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:115)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:45)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:82)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:637)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:410)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:509)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:361)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:402)\r\n\tat objectrepository.Login_page.username(Login_page.java:21)\r\n\tat stepdefinition.Steps.iInputUserNameAndPassword(Steps.java:131)\r\n\tat ✽.And I Input User Name \"Admin\" and Password \"admin123\"(features/Login.feature:12)\r\n",
-  "status": "failed"
+  "duration": 510541900,
+  "status": "passed"
 });
 formatter.match({
   "location": "Steps.iClickOnSignInButton()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 3571244600,
+  "status": "passed"
 });
 formatter.match({
   "location": "Steps.iVerifyWelcomeLabel()"
 });
 formatter.result({
-  "status": "skipped"
-});
-formatter.after({
-  "duration": 1268243700,
+  "duration": 6174433800,
   "status": "passed"
 });
 formatter.after({
-  "duration": 3205400,
+  "duration": 1560535200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 46500,
   "status": "passed"
 });
 });
